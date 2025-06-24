a4481d64559cb7bc7b1c4adb4a231b03c70c8679
aloneen <seisenbek2005@mail.ru>
2025-06-24 18:32:39 +0500

    First initial commit

---
+++ src/main/java/kz/seisen/kormeback/KormeBackApplication.java
@@ -0,0 +1,13 @@
+package kz.seisen.kormeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KormeBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KormeBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/seisen/kormeback/KormeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.seisen.kormeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KormeBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
