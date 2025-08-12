a7d9dbc055ac4cb299b0e4cf8c299de0b387d8b0
aloneen <seisenbek2005@mail.ru>
2025-08-12 15:20:20 +0500

    Created User repo and service

--- src/main/java/kz/seisen/kormeback/repository/UserRepository.java
+++ src/main/java/kz/seisen/kormeback/repository/UserRepository.java
@@ -3,6 +3,9 @@ package kz.seisen.kormeback.repository;
 import kz.seisen.kormeback.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
